3a133fcd9d983897e4ff12116070b312a9b1b81c
psharma <psharma>
2001-08-23 18:25:56 +0000

    *** empty log message ***

--- qos/src/org/cougaar/core/qos/quo/RSSLink.java
+++ qos/src/org/cougaar/core/qos/quo/RSSLink.java
@@ -28,12 +28,10 @@ import com.bbn.quo.data.DataValue;
 import com.bbn.quo.data.HostDS;
 import com.bbn.quo.data.RSS;
 
-import org.cougaar.core.mts.RMILinkProtocol;
 import org.cougaar.core.society.MessageAddress;
 import org.cougaar.core.mts.NameSupport;
 import org.cougaar.core.qos.monitor.ResourceMonitorServiceImpl;
 
-import java.rmi.Remote;
 import java.util.Observable;
 
 public class RSSLink extends ResourceMonitorServiceImpl
@@ -47,40 +45,9 @@ public class RSSLink extends ResourceMonitorServiceImpl
 	rss = RSS.makeInstance(propfile);
     }
 
-    private String extractHost (String string) {
-	// form is
-	// classname[RemoteStub [ref: [endpoint:[host:port](local),objID:[0]]]]
-	int type_end = string.indexOf('[');
-	if (type_end < 0) return null;
-	String type = string.substring(0, type_end);
-	
-	// Local stubs of remote objects will have _Stub appended to
-	// the type, but we don't want that here.
-	if (type.endsWith("_Stub")) {
-	    type  = type.substring(0, type.length()-5);
-	    // System.out.println("//// Removed _stub: " + type);
-	}
-
-
-	int host_start = string.indexOf("[endpoint:[");
-	if (host_start < 0) return null;
-	host_start += 11;
-	int host_end = string.indexOf(':', host_start);
-	if (host_end < 0) return null;
-	return string.substring(host_start, host_end);
-    }    
-
-
-
-    private String addressToHost(MessageAddress agentAddress) {
-	String ttype = RMILinkProtocol.PROTOCOL_TYPE;
-	Object stub = nameSupport.lookupAddressInNameServer(agentAddress, ttype);
-	if (stub == null || !(stub instanceof Remote)) return null;
-	return extractHost(stub.toString());
-    }
 
     private DataFormula jips(MessageAddress agentAddress) {
-	String host = addressToHost(agentAddress);
+	String host = getHostForAgent(agentAddress);
 	if (host == null) return null;
 	String[] args = { host };
 	DataScopeSpec[] path = new DataScopeSpec[1];
@@ -93,7 +60,7 @@ public class RSSLink extends ResourceMonitorServiceImpl
 	DataFormula jips = jips(agentAddress);
 	if (jips != null) {
 	    DataValue value = jips.query();
-	    System.out.println("===== jps DataValue=" + value);
+	    System.out.println("===== jips DataValue=" + value);
 	    return value.getDoubleValue();
 	} else {
 	    return 0.0;
