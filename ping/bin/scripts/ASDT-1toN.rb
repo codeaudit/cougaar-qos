dab9f838fa9baebf15c7b5ef2a3162ecf2d4ce99
jwong <jwong>
2004-03-18 20:52:00 +0000

    Moving our stuff to qos/ping project. (DDP)

---
+++ ping/bin/scripts/ASDT-1toN.rb
@@ -0,0 +1,27 @@
+=begin experiment
+
+name: ASDT-1toN-MiniPing
+description: Run MiniPing with the ASDT configuration.  Single source, multiple sinks.
+script: MiniPing-TEMPLATE.rb
+parameters:
+ - config_name: ASDT-1toN
+ - strategy: CreateSrcToMultSinkPing
+ - ping_pairs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 35, 40, 45, 50]
+ - rules:
+   - $CIP/csmart/config/rules/isat
+   - $CIP/csmart/config/rules/yp
+   - $CIP/configs/ping-dynamic/fast-startup.rule 
+   - $CIP/csmart/config/rules/security
+ - community_rules:
+   - $CIP/csmart/config/rules/security/communities
+ - sleep_time: 600
+ - sleep_delta: 0
+=end
+CIP = ENV['CIP']
+
+$:.unshift File.join(CIP, 'csmart', 'acme_scripting', 'src', 'lib')
+$:.unshift File.join(CIP, 'csmart', 'acme_service', 'src', 'redist')
+$:.unshift File.join(CIP, 'configs')
+
+require 'cougaar/scripting'
+Cougaar::ExperimentDefinition.register(__FILE__)
