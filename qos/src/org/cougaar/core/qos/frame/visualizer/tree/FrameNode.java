821928e64b250437eff8732c271b8aa0d224eefe
jzinky <jzinky>
2005-05-17 17:15:57 +0000

    FrameNode fix

--- qos/src/org/cougaar/core/qos/frame/visualizer/tree/FrameNode.java
+++ qos/src/org/cougaar/core/qos/frame/visualizer/tree/FrameNode.java
@@ -44,6 +44,7 @@ public class FrameNode extends DefaultMutableTreeNode {
         this.frame = frame;
         this.relationship = null;
         this.relationshipNodes = new HashMap();
+        this.cachedChildren = new HashSet();
         this.proxies = new ArrayList();
         this.label = (frame == null ? "" : (frame instanceof PrototypeFrame ? ((PrototypeFrame)frame).getName() : (String) frame.getValue("name")));
     }
