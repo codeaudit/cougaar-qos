1edd6792e6f6da41248ba0b63228236b1785a2b5
jzinky <jzinky>
2006-06-26 16:50:25 +0000

    Prevent inappropriate get* methods from being included in BeanInfo

---
+++ qos/src/org/cougaar/core/qos/frame/FrameBeanInfo.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2006 BBN Technologies Corp.  All Rights Reserved. 
+ *
+ * $Id: FrameBeanInfo.java,v 1.1 2006-06-26 16:50:25 jzinky Exp $
+ */
+package org.cougaar.core.qos.frame;
+
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
+
+/**
+ * Suppress all bean info
+ *
+ */
+public class FrameBeanInfo extends SimpleBeanInfo {
+    public PropertyDescriptor[] getPropertyDescriptors() {
+	return new PropertyDescriptor[] {};
+    }
+}
