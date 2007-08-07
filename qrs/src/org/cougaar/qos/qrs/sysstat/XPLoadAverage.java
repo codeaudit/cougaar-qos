a802cce14bd53ce7a6dfa3af2b44509c7eefed98
jzinky <jzinky>
2007-08-07 18:23:22 +0000

    Get a pseudo load average for XP via typeperf

---
+++ qrs/src/org/cougaar/qos/qrs/sysstat/XPLoadAverage.java
@@ -0,0 +1,60 @@
+/*
+ * =====================================================================
+ * (c) Copyright 2007 BBN Technologies
+ * =====================================================================
+ */
+
+package org.cougaar.qos.qrs.sysstat;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.cougaar.qos.qrs.DataValue;
+
+public class XPLoadAverage extends SysStatHandler {
+    private static final String command = 
+	"typeperf \"\\Processor(_total)\\% Processor Time\" -sc 1";
+
+    // private static final DataValue ClockSpeed =
+    // new DataValue(Hardware.getClockSpeed(), SECOND_MEAS_CREDIBILITY);
+
+    private String key;
+
+    public void initialize(String host, int pid) {
+        key = "Host" + KEY_SEPR + host + KEY_SEPR + "CPU" + KEY_SEPR + "loadavg";
+        // clock_key = "Host" + KEY_SEPR + host +
+        // KEY_SEPR + "CPU" + KEY_SEPR + "clockspeed";
+    }
+
+    public void getData(Map<String, DataValue> map) {
+        Logger logger = org.cougaar.qos.qrs.Logging.getLogger(XPLoadAverage.class);
+        String line = null;
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            InputStream stdOut = process.getInputStream();
+            InputStreamReader rdr = new InputStreamReader(stdOut);
+            BufferedReader bufferedStdOut = new BufferedReader(rdr);
+            line = bufferedStdOut.readLine(); // handy blank line -- ignore
+            line = bufferedStdOut.readLine(); //headers --- ignore
+            line = bufferedStdOut.readLine();
+            bufferedStdOut.close();
+            process.destroy();
+        } catch (java.io.IOException ioex) {
+            logger.warn("Error running typeperf: " + ioex.getMessage());
+            return;
+        }
+
+        String[] csv = line.split(",");
+        if (csv.length == 2) {
+            String percentUtil = csv[1];
+            // remove the extra pair of surrounding quotes
+            percentUtil = percentUtil.substring(1, percentUtil.length()-2);
+            double loadavg = Double.parseDouble(percentUtil) / 100.0; // XXX: Phony load avg
+            map.put(key, new DataValue(loadavg, SECOND_MEAS_CREDIBILITY, "", PROVENANCE));
+        }
+    }
+
+}
