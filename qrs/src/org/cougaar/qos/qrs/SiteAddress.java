5d7afa3046ba4d08fe936dfd5a506ed70799cb88
jzinky <jzinky>
2007-10-18 21:04:32 +0000

    Mask constants must be specified explicitly as longs

--- qrs/src/org/cougaar/qos/qrs/SiteAddress.java
+++ qrs/src/org/cougaar/qos/qrs/SiteAddress.java
@@ -37,17 +37,27 @@ import org.cougaar.util.log.Logger;
 public class SiteAddress {
     private static Map<String, SiteAddress> sites = new HashMap<String, SiteAddress>();
 
-    private static final long[] Masks =
-            {0x00000000, 0x80000000, 0xC0000000, 0xE0000000, 0xF0000000, 0xF8000000, 0xFC000000,
-                    0xFE000000, 0xFF000000, 0xFF800000, 0xFFC00000, 0xFFE00000, 0xFFF00000,
-                    0xFFF80000, 0xFFFC0000, 0xFFFE0000, 0xFFFF0000, 0xFFFF8000, 0xFFFFC000,
-                    0xFFFFE000, 0xFFFFF000, 0xFFFFF800, 0xFFFFFC00, 0xFFFFFE00, 0xFFFFFF00,
-                    0xFFFFFF80, 0xFFFFFFC0, 0xFFFFFFE0, 0xFFFFFFF0, 0xFFFFFFF8, 0xFFFFFFFC,
-                    0xFFFFFFFE, 0xFFFFFFFF};
+    private static final long[] Masks = {
+        0x00000000l, 0x80000000l, 0xC0000000l, 0xE0000000l,
+        0xF0000000l, 0xF8000000l, 0xFC000000l, 0xFE000000l, 
+        0xFF000000l, 0xFF800000l, 0xFFC00000l, 0xFFE00000l, 
+        0xFFF00000l, 0xFFF80000l, 0xFFFC0000l, 0xFFFE0000l, 
+        0xFFFF0000l, 0xFFFF8000l, 0xFFFFC000l, 0xFFFFE000l, 
+        0xFFFFF000l, 0xFFFFF800l, 0xFFFFFC00l, 0xFFFFFE00l,
+        0xFFFFFF00l, 0xFFFFFF80l, 0xFFFFFFC0l, 0xFFFFFFE0l,
+        0xFFFFFFF0l, 0xFFFFFFF8l, 0xFFFFFFFCl, 0xFFFFFFFEl,
+        0xFFFFFFFFl
+    };
 
     public long net;
     private final long net_masked;
     public long mask;
+    
+    public SiteAddress(long address, long mask) {
+        this.net = address;
+        this.mask = mask;
+        net_masked = mask & net;
+    }
     
     public SiteAddress(byte[] address, int maskPrefixLength) {
         this.net = bytesToLongAddress(address);
@@ -122,7 +132,8 @@ public class SiteAddress {
 
     public static int maskToPrefixLength(long mask) {
         for (int i = 0; i < Masks.length; i++) {
-            if (mask == Masks[i]) {
+            long l = Masks[i];
+            if (mask == l) {
                 return i;
             }
         }
