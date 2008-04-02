83dfd97ff386009287875f11dd202260ce733062
jzinky <jzinky>
2008-04-02 13:42:18 +0000

    The start of a general statistics package that can output to CSV files

---
+++ qos/src/org/cougaar/core/qos/stats/Anova.java
@@ -0,0 +1,185 @@
+/* =============================================================================
+ *
+ *                  COPYRIGHT 2007 BBN Technologies Corp.
+ *                  10 Moulton St
+ *                  Cambridge MA 02138
+ *                  (617) 873-8000
+ *
+ *       This program is the subject of intellectual property rights
+ *       licensed from BBN Technologies
+ *
+ *       This legend must continue to appear in the source code
+ *       despite modifications or enhancements by any party.
+ *
+ *
+ * =============================================================================
+ *
+ * Created : Aug 14, 2007
+ * Workfile: Anova.java
+ * $Revision: 1.1 $
+ * $Date: 2008-04-02 13:42:18 $
+ * $Author: jzinky $
+ *
+ * =============================================================================
+ */
+
+package org.cougaar.core.qos.stats;
+
+public class Anova implements Statistic {
+    private String name;
+    private int valueCount;
+    private double sum;
+    private double sumSq;
+    private double max; 
+    private double min;
+    private long startTime ;
+    private long endTime ;
+
+    Anova(String name) {
+        reset();
+        this.name = name;
+    }
+    
+    public void reset() {
+        valueCount = 0;
+        sum = 0.0;
+        sumSq = 0.0;
+        max = Double.MIN_VALUE;
+        min = Double.MAX_VALUE;
+        startTime = System.currentTimeMillis();
+        endTime = startTime;
+
+    }
+
+    public void newValue(long value) {
+    	newValue((double)value);
+    }
+    
+    public void newValue(double value) {
+        ++valueCount;
+        sum += value;
+        sumSq += value * value;
+        max = Math.max(max, value);
+        min = Math.min(min, value);
+        endTime = System.currentTimeMillis();
+    }
+    
+    public Anova delta(Statistic stat) {
+        Anova other = (Anova) stat;
+        Anova delta = new Anova(null);
+        delta.name=name;
+        delta.valueCount = valueCount - other.valueCount;
+        delta.sum = sum - other.sum;
+        delta.sumSq = sumSq - other.sumSq;
+        delta.max = Math.max(max, other.max);
+        delta.min = Math.min(min, other.min);
+        delta.startTime= other.endTime;
+        delta.endTime = endTime;
+        return delta;
+    }
+    
+    public void accumulate(Statistic stat) {
+        Anova other = (Anova) stat;
+        valueCount += other.valueCount;
+        sum += other.sum;
+        sumSq += other.sumSq;
+        max = Math.max(max, other.max);
+        min = Math.min(min, other.min);
+        startTime= Math.min(startTime, other.startTime);
+        endTime= Math.max(endTime, other.endTime);
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getSummaryString() {
+		StringBuffer buf = new StringBuffer();
+		buf.append(getName());
+		buf.append(":");
+		buf.append(min());
+		buf.append("/");
+		buf.append(average());
+		buf.append("/");
+		buf.append(max());
+		return buf.toString();
+	}
+         
+    public String toString() {
+    	return "<Anova: " + getSummaryString() + ">";
+    }
+    
+    public Anova clone() throws CloneNotSupportedException {
+        return (Anova) super.clone();
+    }   
+    
+    public Anova snapshot() {
+    	Anova copy;
+		try {
+			copy = clone();
+		} catch (CloneNotSupportedException e) {
+			// This can't happen
+			return null;
+		}
+    	copy.endTime = System.currentTimeMillis();
+    	return copy;
+    }
+    
+    public StatisticKind getKind () {
+    	return StatisticKind.ANOVA;
+    }
+    
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public long getEndTime() {
+		return endTime;
+	}
+    
+    /*
+     * Anova specific statistical output
+     */
+    
+    public int getValueCount() {
+        return valueCount;
+    }
+    
+    public double getSum() {
+        return sum;
+    }
+    
+    public double itemPerSec() {
+    	long deltaT = endTime - startTime;
+        if (deltaT != 0) {
+            return (1000.0 * valueCount)/deltaT;
+        } else {
+            return -1d;
+        }
+    }
+    
+    public double average(){
+        if (valueCount != 0) {
+            return sum / valueCount;
+        } else {
+            return -1;
+        }
+    }
+    
+    public double max() {
+        return max;
+    }
+    
+    public double min() {
+        return min;
+    }
+    
+    public double stdDev() {
+        if (valueCount !=0) {
+            double avgSquared = Math.pow(average(), 2);
+            return  Math.sqrt(avgSquared - sumSq );
+        }
+        return 0;
+    }
+
+ }
---
+++ qos/src/org/cougaar/core/qos/stats/Statistic.java
@@ -0,0 +1,106 @@
+/* =============================================================================
+*
+*                  COPYRIGHT 2007 BBN Technologies Corp.
+*                  10 Moulton St
+*                  Cambridge MA 02138
+*                  (617) 873-8000
+*
+*       This program is the subject of intellectual property rights
+*       licensed from BBN Technologies
+*
+*       This legend must continue to appear in the source code
+*       despite modifications or enhancements by any party.
+*
+*
+* =============================================================================
+*
+* Created : Aug 14, 2007
+* Workfile: Statistic.java
+* $Revision: 1.1 $
+* $Date: 2008-04-02 13:42:18 $
+* $Author: jzinky $
+*
+* =============================================================================
+*/
+ 
+package org.cougaar.core.qos.stats;
+
+import java.io.Serializable;
+
+/*
+ * Statistic gathers a summary of a series of statistical values.
+ * Each statistic has its own interface for the actual data summary that it is collecting
+ * Only the mechanisms for processing statistics in general are exposed in this interface:
+ * Several types of operations are performed on statistic be different roles
+ * 1) Collector initialize the statistic (reset) and tags the statistic with the collection point name
+ * 2) Collector puts  values into statistics (new value)
+ * 3) Report Generator snap shots statistics (clone) at two points puts in time
+ *   and sends the (delta)  between two snapshots
+ * 4) Controller (accumulate) statistics from multiple collectors
+ * 5) Controller (store) statistic for later post processing
+ */
+
+public interface Statistic extends Cloneable, Serializable {
+	
+	/*
+	 * Add a new value to the statistic
+	 */
+	public void newValue(long value);
+	
+	public void newValue(double value);
+
+	/*
+	 * Return Statistic to initial state
+	 */
+    public void reset();
+    
+    /*
+     * Take two samples of the statistic in time and return a new statistic representing 
+     * only the newValue events that happened between the two samples.
+     */
+    public Statistic delta(Statistic earlierStatistic);
+    
+    /* 
+     * Accumulate additional statistics into overview statistic
+     */
+    public void accumulate(Statistic additionalStatistic);
+ 
+    /*
+     * Copy the content of the statistics, leaving endTime alone;
+     */
+    public Statistic clone() throws CloneNotSupportedException;
+    
+    /*
+     * Snapshot the content of the statistics, setting endTime to the current time;
+     */
+    public Statistic snapshot();
+ 
+    
+	/*
+	 * Get name of the collection point where the statistic was gathered.
+	 * The name should represent a unique collection point over the whole society.
+	 * The name is used to bind statistic collected at the same collection point, 
+	 * but at different times
+	 */
+	public String getName();
+	
+	/*
+	 * Short one-line summary string for human consumption
+	 */
+    public String getSummaryString();
+    
+    /*
+     * Return enum for use in switch statements
+     */
+    public StatisticKind getKind();
+    
+    /*
+     * Time statistics started (not the time of the first newValue)
+     */    
+	public long getStartTime();
+	
+	/* 
+	 * Time of the last newValue or time the snapshot was taken
+	 */
+	public long getEndTime(); 
+}
