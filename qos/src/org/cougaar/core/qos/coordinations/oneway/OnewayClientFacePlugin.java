619fb3405ddd178fc09d91a1fbeaa16c23ebc183
jzinky <jzinky>
2008-09-19 12:52:27 +0000

    2nd pass at Oneway coordination

--- qos/src/org/cougaar/core/qos/coordinations/oneway/OnewayClientFacePlugin.java
+++ qos/src/org/cougaar/core/qos/coordinations/oneway/OnewayClientFacePlugin.java
@@ -16,80 +16,46 @@
  *
  * Created : Sept 18, 2008
  * Workfile: OnewayServerFacePlugin
- * $Revision: 1.1 $
- * $Date: 2008-09-18 20:10:48 $
+ * $Revision: 1.2 $
+ * $Date: 2008-09-19 12:52:27 $
  * $Author: jzinky $
  *
  * =============================================================================
  */
 package org.cougaar.core.qos.coordinations.oneway;
 
-import org.cougaar.core.mts.AttributeConstants;
-import org.cougaar.core.mts.MessageAddress;
-import org.cougaar.core.mts.MessageAddressWithAttributes;
-import org.cougaar.core.mts.MessageAttributes;
-import org.cougaar.core.mts.SimpleMessageAttributes;
 import org.cougaar.core.qos.coordinations.Face;
 import org.cougaar.core.qos.coordinations.FacePlugin;
 import org.cougaar.core.relay.SimpleRelay;
-import org.cougaar.core.relay.SimpleRelaySource;
-import org.cougaar.core.util.UID;
 import org.cougaar.core.util.UniqueObject;
 import org.cougaar.util.annotations.Cougaar;
 import org.cougaar.util.annotations.Subscribe;
 
 /**
- * The One way Server Face pulls matched objects off the blackboard and sends them to the Client.
- * No reply is expected from the client. 
- * Each object is guaranteed its own message.
- * The objects can be dropped along the way.
- * Optionally, the object can be removed from the black board
- * 
- * Subclasses should implement {@link #match}.
- */
-abstract public class OnewayClientFacePlugin extends FacePlugin<OneWay.Server>
+* The One way Client Face waits for relays to appear on its blackboard and pulls the content out.
+* No reply is sent back to  Server
+* 
+* Subclasses should implement {@link #match}.
+*/
+
+//TODO the sender has to mark to object so it is ours to unwrap
+//maybe use match function
+//maybe look for UID in target address
+//maybe generic, cleint for all Oneways.
+abstract public class OnewayClientFacePlugin extends FacePlugin<OneWay.Client>
     implements OneWay.Matcher<Face<OneWay.EventType>> {
     
     public OnewayClientFacePlugin() {
-        super(new OneWay.Server());
+        super(new OneWay.Client());
     }
-    @Cougaar.Arg(name="clientName", required=true)
-    public MessageAddress clientAddress;
-    
-    @Cougaar.Arg(name="defaultTimoutMillis", defaultValue="1000")
-    public int defaultTimoutMillis;
-    
-    @Cougaar.Arg(name="deleteOnSend", defaultValue="true")
-    public boolean deleteOnSend;
    
-    @Cougaar.Execute(on=Subscribe.ModType.ADD, when="isSendObject")
-    public void executeNewObjectToSend(Object objectToSend) {
-        UID uid = uids.nextUID();
-        // make the target address with attributes
-        MessageAttributes attrs = new SimpleMessageAttributes();
-        // TODO add priority and other QoS attributes
-        attrs.setAttribute(AttributeConstants.MESSAGE_SEND_TIMEOUT_ATTRIBUTE, defaultTimoutMillis);
-        //HACK: Relay Directives are put into different MTS messages, if their target is different
-        // We add an unique attribute value to the target address
-        // so that the relay is guaranteed to go in its own message
-        attrs.setAttribute("UID", uid);
-        MessageAddress targetAddress =
-                MessageAddressWithAttributes.getMessageAddressWithAttributes(clientAddress, attrs);
-       
-        // Make relay that will be sent once and 
-        SimpleRelay relay = new SimpleRelaySource(uid, agentId, targetAddress, null);
-        relay.setQuery(objectToSend);
-        blackboard.publishAdd(relay);
-        //Cleanup: 
-        //delete relay right away
-        blackboard.publishRemove(relay);
-        // Remove blackboard object itself
-        if (deleteOnSend) {
-            blackboard.publishRemove(objectToSend);
-        }
-    }
+    @Cougaar.Execute(on=Subscribe.ModType.ADD, when="isMyRelay")
+    public void executeNewObjectToSend(SimpleRelay relay) {
+       Object object = relay.getQuery();
+        blackboard.publishAdd(object);
+     }
 
-    public boolean isSendableObject(UniqueObject object) {
-        return match(OneWay.EventType.SEND, object);
+    public boolean isMyRelay(UniqueObject object) {
+        return match(OneWay.EventType.RECEIVE, object);
     }
 }
--- qos/src/org/cougaar/core/qos/coordinations/oneway/OnewayServerFacePlugin.java
+++ qos/src/org/cougaar/core/qos/coordinations/oneway/OnewayServerFacePlugin.java
@@ -16,46 +16,88 @@
  *
  * Created : Sept 18, 2008
  * Workfile: OnewayServerFacePlugin
- * $Revision: 1.1 $
- * $Date: 2008-09-18 20:10:48 $
+ * $Revision: 1.2 $
+ * $Date: 2008-09-19 12:52:27 $
  * $Author: jzinky $
  *
  * =============================================================================
  */
 package org.cougaar.core.qos.coordinations.oneway;
 
+import org.cougaar.core.mts.AttributeConstants;
+import org.cougaar.core.mts.MessageAddress;
+import org.cougaar.core.mts.MessageAddressWithAttributes;
+import org.cougaar.core.mts.MessageAttributes;
+import org.cougaar.core.mts.SimpleMessageAttributes;
 import org.cougaar.core.qos.coordinations.Face;
 import org.cougaar.core.qos.coordinations.FacePlugin;
 import org.cougaar.core.relay.SimpleRelay;
+import org.cougaar.core.relay.SimpleRelaySource;
+import org.cougaar.core.util.UID;
 import org.cougaar.core.util.UniqueObject;
 import org.cougaar.util.annotations.Cougaar;
 import org.cougaar.util.annotations.Subscribe;
 
 /**
- * The One way Client Face waits for relays to appear on its blackboard and pulls the content out.
- * No reply is sent back to  Server
+ * The One way Server Face pulls matched objects off the blackboard and sends them to the Client.
+ * No reply is expected from the client. 
+ * Each object is guaranteed its own message.
+ * The objects can be dropped along the way.
+ * Optionally, the object can be removed from the black board
  * 
  * Subclasses should implement {@link #match}.
  */
 
-// TODO the sender has to mark to object so it is ours to unwrap
-// maybe use match function
-// maybe look for UID in target address
-// maybe generic, cleint for all Oneways.
 abstract public class OnewayServerFacePlugin extends FacePlugin<OneWay.Server>
     implements OneWay.Matcher<Face<OneWay.EventType>> {
+	 public static final String UID_ATTRIBUTE ="UID";
     
-    public OnewayServerFacePlugin() {
-        super(new OneWay.Server());
-    }
-   
-    @Cougaar.Execute(on=Subscribe.ModType.ADD, when="isMyRelay")
-    public void executeNewObjectToSend(SimpleRelay relay) {
-       Object object = relay.getQuery();
-        blackboard.publishAdd(object);
-     }
+	 public OnewayServerFacePlugin() {
+	        super(new OneWay.Server());
+	    }
+	    @Cougaar.Arg(name="clientName", required=true)
+	    public MessageAddress clientAddress;
+	    
+	    @Cougaar.Arg(name="defaultTimeoutMillis", defaultValue="1000")
+	    public int defaultTimeoutMillis;
+	    
+	    @Cougaar.Arg(name="deleteOnSend", defaultValue="true")
+	    public boolean deleteOnSend;
+	   
+	    @Cougaar.Execute(on=Subscribe.ModType.ADD, when="isSendableObject")
+	    public void executeNewObjectToSend(Object objectToSend) {
+	        UID uid = uids.nextUID();
+	        MessageAddress targetAddress = makeMessageAddress(objectToSend, clientAddress, defaultTimeoutMillis, uid);
+	        // Make relay that will be sent once
+	        SimpleRelay relay = new SimpleRelaySource(uid, agentId, targetAddress, null);
+	        relay.setQuery(objectToSend);
+	        blackboard.publishAdd(relay);
+	        //Cleanup: 
+	        //delete relay right away
+	        blackboard.publishRemove(relay);
+	        // Remove blackboard object itself
+	        if (deleteOnSend) {
+	            blackboard.publishRemove(objectToSend);
+	        }
+	    }
+	    
+	    /*
+	     * Override this method to use properties of the objectToSend to change the attributes of the target address.
+	     */
+	    protected MessageAddress makeMessageAddress( Object objectToSend, MessageAddress clientAddress, 
+	    		int defaultTimeoutMillis, UID uid) {
+	        // make the target address with attributes
+	        MessageAttributes attrs = new SimpleMessageAttributes();
+	        // TODO add priority and other QoS attributes
+	        attrs.setAttribute(AttributeConstants.MESSAGE_SEND_TIMEOUT_ATTRIBUTE, defaultTimeoutMillis);
+	        //HACK: Relay Directives are put into different MTS messages, if their target is different
+	        // We add an unique attribute value to the target address
+	        // so that the relay is guaranteed to go in its own message
+	        attrs.setAttribute(UID_ATTRIBUTE, uid);
+	        return  MessageAddressWithAttributes.getMessageAddressWithAttributes(clientAddress, attrs);
+	    }
 
-    public boolean isMyRelay(UniqueObject object) {
-        return match(OneWay.EventType.RECEIVE, object);
-    }
+	    public boolean isSendableObject(UniqueObject object) {
+	        return match(OneWay.EventType.SEND, object);
+	    }
 }
