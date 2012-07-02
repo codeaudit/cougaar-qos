8aa054d90abfe6dd53bd6889fb8c54ec8bbb74a3
jzinky <jzinky@bbn.com>
2012-07-02 16:15:58 -0400

    removed ping project and moved to attic repo

--- ping/bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CIP=`pwd`
-export CIP
-COUGAAR_INSTALL_PATH=`pwd`
-export COUGAAR_INSTALL_PATH
-ping/bin/jips.sh
-cd ping/configs/ping
-../../bin/Node-min MiniPing-minimum.xml NodeA
--- ping/configs/ping-dynamic/scripting.rb
+++
@@ -1,5 +0,0 @@
-require 'ping-dynamic/ping-db'
-require 'ping-dynamic/PingSocietyBuilderUtils'
-require 'ping-dynamic/stressors.rb'
-#require 'ping-dynamic/upload_network_metrics'
-#require 'ping-dynamic/networkXMLtoMetricParser'
